a7106722b7335fdcce2fed03998217d8f1644b5c
briddo <briddo@gmail.com>
2025-11-10 19:13:12 +0000

    made tdd tests pass

--- interview/order/urls.py
+++ interview/order/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from interview.order.views import OrderListCreateView, OrderTagListCreateView
+from interview.order.views import (
+    OrderDateRangeView,
+    OrderListCreateView,
+    OrderTagListCreateView,
+)
 
 
 urlpatterns = [
+    path("date-range/", OrderDateRangeView.as_view(), name="order-date-range"),
     path("tags/", OrderTagListCreateView.as_view(), name="order-detail"),
     path("", OrderListCreateView.as_view(), name="order-list"),
 ]
